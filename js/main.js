fad2fb488901f8063134557e5cf7ef7acbe25838
Joel Burlingame <jtburlingame777@gmail.com>
2025-05-19 16:20:23 -0700

    added the begginings of projects section with some place holder

--- js/main.js
+++ js/main.js
@@ -2,4 +2,5 @@ import '../css/styles.css';
 import '../css/components/header.css';
 import '../css/components/intro.css';
 import '../css/components/about.css';
+import '../css/components/work.css';
 import '../css/utils.css';
